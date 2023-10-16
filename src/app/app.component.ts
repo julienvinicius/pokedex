9b96c61300020a3eab6ad7f8119dc028d18fb167
julienvinicius <gorguetmoura@gmail.com>
2023-10-16 10:38:07 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: `<router-outlet></router-outlet> `,
+})
+export class AppComponent {
+  title = 'pokedex';
+}
